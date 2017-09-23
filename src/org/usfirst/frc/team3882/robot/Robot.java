b6b3c1a3765539ba099c3888160db747114f6864
JAY <jaybaptista@JaysMBP.local>
2017-09-23 11:54:19 -1000

    theta?
    theta.

--- src/org/usfirst/frc/team3882/robot/Robot.java
+++ src/org/usfirst/frc/team3882/robot/Robot.java
@@ -1,10 +1,21 @@
 package org.usfirst.frc.team3882.robot;
 
+import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.cscore.VideoMode;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Range;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.CvSource;
 import edu.wpi.cscore.MjpegServer;
 
 public class Robot extends IterativeRobot {
@@ -18,13 +29,28 @@ public class Robot extends IterativeRobot {
 		chooser.addDefault("Default Auto", defaultAuto);
 		chooser.addObject("My Auto", customAuto);
 		SmartDashboard.putData("Auto choices", chooser);
+
+		new Thread(() -> {
+            UsbCamera cam0 = CameraServer.getInstance().startAutomaticCapture("cam0", 0); //grabs mscam
+            UsbCamera cam1 = CameraServer.getInstance().startAutomaticCapture("cam1", 1); //grabs mscam
+            MjpegServer server = new MjpegServer("server", 1181); //Sets port to stream server on 1181 -- within legal limits.
+            cam0.setResolution(640, 480); 
+            CvSink sink = CameraServer.getInstance().getVideo("cam0");
+            CvSource outStream = CameraServer.getInstance().putVideo("Blur", 640, 480);
+            double[] hue = {43.70503597122302, 81.70648464163823};
+            double[] sat = {43.57014388489208, 163.61774744027304};
+            double[] lum = {43.57014388489208, 220.1877133105802};
+            Mat s0 = new Mat();
+            Mat out = new Mat();
+            while(!Thread.interrupted()) {
+                sink.grabFrame(s0);
+                //Pipeline begins here:
+                Theta.vidProc(s0, hue, sat, lum);
+                outStream.putFrame(out);
+                server.setSource(outStream);
+            }
+        }).start();
 		
-		// Instantiate Camera Server
-		UsbCamera usbCam = new UsbCamera("mscam", 0);
-		MjpegServer server = new MjpegServer("server", 1180);
-		
-		usbCam.setVideoMode(VideoMode.PixelFormat.kMJPEG, 480, 320, 16);
-		server.setSource(usbCam);
 	}
 
 	@Override
---
+++ src/org/usfirst/frc/team3882/robot/Theta.java
@@ -0,0 +1,30 @@
+package org.usfirst.frc.team3882.robot;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.CvSource;
+import edu.wpi.cscore.CameraServerJNI;
+
+public class Theta {
+
+	public static Mat vidProc(Mat s0, double[] hue, double[] sat, double[] lum) {
+        Scalar lower = new Scalar(hue[0], sat[0], lum[0]);
+        Scalar upper = new Scalar(hue[1], sat[1], lum[1]);
+        Mat out = new Mat();
+        Mat s1 = new Mat();
+        Mat s2 = new Mat();
+        Mat s3 = new Mat();
+        //Color Filtering
+		Imgproc.cvtColor(s0, s1, Imgproc.COLOR_BGR2HLS);
+        Core.inRange(s1, lower, upper, s2);
+        //Filtering and Cleaning Up Noise
+        Imgproc.erode(s2, s3, new Mat(2,2,2), new Point(0,0), 2);
+        Imgproc.dilate(s2, s3, new Mat(2,2,2), new Point(0,0), 2);
+        return out;
+	}
+   }
